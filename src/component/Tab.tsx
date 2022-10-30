10575461b3c8954d5a11b18ff8cf6230e1bc8057
Leo <heesu12200@gmail.com>
2022-10-30 13:31:04 +0900

    add function -> switch tab -> place button for manual addition

--- src/component/Tab.tsx
+++ src/component/Tab.tsx
@@ -9,23 +9,31 @@ let margin = 'mr-2';
 
 // text color, text size, text align
 let text = 'text-blue-400 text-lg text-center';
+let textDisabled = 'text-black text-lg text-center';
+let hover = 'hover:text-blue-400';
 
 let width = 'w-20';
 let border = 'border-t border-r border-l border-gray-400 border-solid rounded-tl-md rounded-tr-md shadow-lg';
+
 
 interface TapProps {
     id: string,
     name: string,
     url: string
+    setEnable?: any,
+    isEnable?: boolean
 }
 
 export default (props: TapProps) => {
 
-
-    const tabClass = [width, bgColor, display, margin, text, border].join(" ");
+    // class for activated tab.
+    const tabClass = [width, bgColor, display, margin, text, border].join(' ');
+    // class for not activated tabs.
+    const disabledClass = [width, tabClass, display, margin, textDisabled, border, hover].join(' ');
     return(
-        <div id={props.id} className={ tabClass } onClick={() => {}}>
-           <Link to={props.url}>{ props.name }</Link>
+        <div id={props.id} className={ props.isEnable? tabClass : disabledClass } onClick={() => {props.setEnable(props.id)}}>
+           <Link to={props.url}>{ props.name } </Link>
+
         </div>
     )
 }
--- src/component/template.tsx
+++ src/component/template.tsx
@@ -2,31 +2,73 @@ import '../css/global.css'
 
 import React, { useState } from "react";
 import { BrowserRouter, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 interface TemplateProps {
     children: React.ReactElement | React.ReactElement[]
     height?: string
+
+    // if true, add function that tabs can be added manually
+    manualAdd?: boolean
 }
+
 
 export default ( props: TemplateProps) => {
 
-    let [selectedTab, changeTab] = useState();
+    const children = props.children;
+
+    let tabHeight;
+    let manualAdd;
 
     if(!props.height) {
-        props.height = '30'
+        tabHeight = '30'
     }
 
+    if(!manualAdd) {
+        manualAdd = true;
+    }
+
+    // if(children.type === React) {
+    //
+    // }
+
+    const [enable, setEnable] = useState<string>('');
+
+    const newChildren = React.Children.map(children, (child, index) => {
+
+        const id = child.props.id;
+        const isEnabled = enable === id;
+
+
+        if(React.isValidElement(child)) {
+
+            return React.cloneElement(child as React.ReactElement,{
+                setEnable: setEnable,
+                isEnable: isEnabled
+            });
+
+        }
+    });
+
+
     return (
-        <div style={{minHeight: '30px', height: props.height + 'px'}} className={"w-full"}>
+        <div style={{minHeight: '30px', height: tabHeight + 'px'}} className={"w-full"}>
             <BrowserRouter>
-                <div className={"h-full"}>
+                <div className={"h-full inline-block"}>
 
-                    { props.children }
+                    { newChildren }
 
                 </div>
-                <div>
-                    ddd
-                </div>
+
+                {
+                    manualAdd &&
+                    <div className={"inline-block cursor-pointer hover:text-blue-400"}>
+                        <FontAwesomeIcon icon={ faPlus } size="xl"></FontAwesomeIcon>
+                    </div>
+
+                }
+
             </BrowserRouter>
         </div>
     )
